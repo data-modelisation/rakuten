c2dccbe7d865e4dcc94d5aa19a4af0b18b527ad9
Karolus <108416147+karolus-git@users.noreply.github.com>
2023-02-02 15:13:20 +0100

    Passage à des transformers pour FE

--- src/tools/text.py
+++ src/tools/text.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from ftlangdetect import detect
 import re
 import logging
+from unidecode import unidecode
+from googletrans import Translator, LANGUAGES
+import swifter 
+
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk.stem.snowball import FrenchStemmer
 from nltk import ngrams, FreqDist
-from . import commons
-from googletrans import Translator, LANGUAGES
-#from translate import Translator
-import swifter 
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+from  sklearn.feature_extraction.text import CountVectorizer
+from  sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler
+
+from . import commons
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
 
 languages = {
         "en": "english",
@@ -24,24 +35,187 @@ languages = {
         "de":"german",
         'it': "italian"
         }
-
-# Selection des 500 mots les plus communs
-NUM_COMMON_WORDS = 500
 lang_abbr = LANGUAGES.keys()
 
-#Traduction d'un text
+french_stopwords = stopwords.words("french") + [
+        'a', 'aur', 'aurion', 'auron', 'avi', 'avion', 'avon', 
+        'ayon', 'dan', 'e', 'etaient', 'etais', 'etait', 'etant',
+        'ete', 'eti', 'etion', 'eum', 'euss', 'eussion', 'fum', 
+        'fuss', 'fussion', 'mem', 'notr', 'ser', 'serion', 'seron', 
+        'soi', 'somm', 'soyon', 'votr'
+    ]
+
+MAX_FEATURES_WORDS = 1000 #Nombre de mots fréquents à conserver
+MAX_TEXT_LENGTH = 300 #Ajustement des textes à cette longueur
+
+class ColumnDropper(BaseEstimator,TransformerMixin):
+
+    def __init__(self, columns=[]):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.copy().drop(self.columns, axis=1)
+
+class LinksMaker(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        X_copy = X.copy()
+        X_copy["links"] = "data/raw/images/image_train/image_" + X_copy.imageid.map(str) + "_product_" + X_copy.productid.map(str) + ".jpg"
+        return X_copy
+        
+class TextColumnMerger(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=[], name="merged_col"):
+        self.columns = columns
+        self.name = name
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        X_copy = X.copy()
+        columns = [X_copy[col] for col in self.columns]
+
+        X_copy[self.name] = commons.merge_columns(columns=columns)
+        return X_copy
+
+class WordsCounter(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=[]):
+        self.columns = columns
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        X_copy = X.copy()
+        for column in self.columns:
+            X_copy[f"words_in_"+column] = X_copy[column].apply(lambda text: get_num_words(text))
+        return X_copy
+
+class LanguageTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, column, text_length=300, translate=False):
+        self.column = column  
+        self.text_length = text_length 
+        self.translate = translate
+
+    def fit(self, X, y=None):
+        return self    
+        
+    def transform(self, X):
+        X_copy = X.copy()
+
+        X_copy["lang"] = X_copy[self.column].apply(lambda text : get_lang(text, text_length=self.text_length))
+        
+        if self.translate:
+            mask_not_fr = X_copy.lang != "fr"
+            X_copy.loc[mask_not_fr, self.column] = X_copy.loc[mask_not_fr].swifter.apply(lambda row: translate_text(row[self.column], src=row.lang, dest="fr"), axis=1)
+            
+        return X_copy
+
+class Vectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, column, type_="tfidf"):
+        self.column = column
+        self.type_ = type_
+        vectorizer_obj = TfidfVectorizer if self.type_=="tf" else CountVectorizer
+        
+        self.vectorizer =  vectorizer_obj(
+                max_features=MAX_FEATURES_WORDS, 
+                ngram_range=(1, 2),                 
+                analyzer="word",
+                preprocessor=clean_text,
+                tokenizer=stemmatize_text,
+                stop_words=french_stopwords
+            )
+        
+    def fit(self, X, y=None):
+
+        self.vectorizer.fit(X[self.column])
+
+        return self    
+        
+    def transform(self, X):
+        print(X.columns)
+        X_copy = X.copy()
+        X_vec = self.vectorizer.transform(X_copy[self.column]).toarray()
+
+        df_vec = pd.DataFrame(
+            X_vec,
+            index=X_copy.index,
+            columns=self.vectorizer.get_feature_names_out()
+        )
+
+        return X_copy.join(df_vec)#, rsuffix= "_" + self.type_)
+
+def build_pipeline():
+
+    pipeline = Pipeline(steps=[
+        ("dropper", ColumnDropper(columns=["links", "productid", "imageid", "lang",])),
+        ("scaler", StandardScaler()),
+        ("classifier", KNeighborsClassifier() )
+    ])
+
+    return pipeline
+
+def build_preprocessor():
+
+    preprocessor = Pipeline(steps=[
+        ('links', LinksMaker()),
+        ('counters', WordsCounter(columns=["designation", "description"])),
+        ('merger', TextColumnMerger(columns=["designation", "description"], name="text")),
+        ('language', LanguageTransformer(column="text", text_length=500, translate=False)),
+        #('vectorize_cn', Vectorizer(column="text", type_="cn")),
+        ('vectorize_tf', Vectorizer(column="text", type_="tf")),
+        ("dropper", ColumnDropper(columns=["designation", "description", "text"])),
+        ])
+
+    return preprocessor
+
+
+def stemmatize_text(text):
+    return [FrenchStemmer().stem(w) for w in word_tokenize(text)]
+
+def clean_text(text):
+
+    if isinstance(text, np.ndarray):
+        text = text[0]
+    
+    #Suppression des balises
+    text = re.sub('<[^<]+?>', ' ', text)
+
+    #Suppression des autres choses que des lettres
+    text = re.sub('[^A-Za-z\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02af\u1d00-\u1d25\u1d62-\u1d65\u1d6b-\u1d77\u1d79-\u1d9a\u1e00-\u1eff\u2090-\u2094\u2184-\u2184\u2488-\u2490\u271d-\u271d\u2c60-\u2c7c\u2c7e-\u2c7f\ua722-\ua76f\ua771-\ua787\ua78b-\ua78c\ua7fb-\ua7ff\ufb00-\ufb06]', ' ', text)
+
+    #Suppression des maj
+    text = text.lower()
+
+    #Suppression des accents
+    text = unidecode(text)
+
+    #Ajustement de la taille des textes
+    # if MAX_TEXT_LENGTH > 0:
+    #     text = text[:MAX_TEXT_LENGTH]
+    
+    return text
+
+#Traduction d'un texte
 def translate_text(text, src="en", dest="fr"):
     if src == "fr":
         return text
     else:
         try:
-            # from_lang = languages.get(src)
-            # translator= Translator(from_lang=from_lang, to_lang="french")
-            # return = translator.translate(text)
-           
+
             translator= Translator()
             translation = translator.translate(text, src=src, dest=dest)
-            return translation.text
+            translated = translation.text
+            #print(src + ":" + text +" ---> " + translated + "\n")
+            return translated
         except Exception as exce:
             print(exce)
             return text
@@ -50,215 +224,27 @@ def translate_text(text, src="en", dest="fr"):
 @commons.timeit
 def translate_serie(df):
     
-    
     return df.swifter.apply(lambda x: translate_text(x.text_clean, src=x.lang, dest="fr"), axis=1)
 
-# Feature-engineering du texte
-@commons.timeit
-def apply_feature_engineering(df:pd.DataFrame, 
-    translate=False,
-    stemm=True):
-    logging.debug("start feature-engineering")
-
-    #Remplacement de Nan par ""
-    df["description"] = df.description.fillna("")
-    logging.debug("nans converted to empty string")
-
-    #Nombre de mots dans designation
-    df["desi_num_words"] = df.designation.apply(lambda x: len(get_text_words(x)))
-    logging.debug(f"mean num words in designation : {df.desi_num_words.mean()}")
-
-    #Nombre de mots dans designation
-    df["desc_num_words"] = df.description.apply(lambda x: len(get_text_words(x)))
-    logging.debug(f"mean num words in designation : {df.desc_num_words.mean()}")
-
-    #Fusion description + designation
-    df["text"] = commons.merge_columns(df.designation, df.description)
-    logging.debug(f"merging of columns")
-
-    #Suppression de la ponctuation
-    df["text_clean"] = remove_balise(df.text)
-    logging.debug(f"balise removed")
-
-    #Langue utilisée
-    df["lang"] = find_language(df.text_clean)
-    logging.debug(f"got the langages")
-
-    #Langue utilisée
-    if translate:
-        not_fr = df.lang != "fr"
-        df.loc[not_fr, "text_clean"] = translate_serie(df.loc[not_fr,:])
-        logging.debug(f"translate the langages")
-
-    #Suppression de la ponctuation
-    df["text_clean"] = remove_punctuation(df.text_clean)
-    logging.debug(f"punctuation removed")
-
-    #Suppression des Maj
-    df["text_clean"] = make_lowercase(df.text_clean)
-    logging.debug(f"text lowered")
-
-    #Suppression des nombres
-    df["text_clean"] = remove_numerical(df.text_clean)
-    logging.debug(f"numbers removed")
-
-    #Nombre de mots dans text
-    df["num_words"] = df.text_clean.apply(lambda x: len(get_text_words(x)))
-    logging.debug(f"mean num words in text : {df.num_words.mean()}")
-
-    #Suppression des stopwords
-    df["text_clean"] = df.apply(lambda x: remove_stops(x.text_clean, x.lang), axis=1)
-    logging.debug(f"stopwords removed")
-
-    # #Suppression des mots inutiles
-    # df["text_clean"] = df.text_clean.apply(lambda x: remove_words(x))
-    # logging.debug(f"useless words removed")
-
-    #Passage à la racine des mots
-    if stemm:
-        df["text_stem"] = stemmatize_serie(df)
-        logging.debug(f"text stemmed")
-    else:
-        df["text_stem"] = df.text_clean
-    
-
-    #Selection des mots les plus courant et chaque mot courant a une colonne 
-    df_dummy, df_commons = select_common_words(df.text_stem)
-    
-    df = pd.concat([df, df_dummy], axis=1)
-
-    return df, df_commons
-
-@commons.timeit
-def select_common_words(serie:pd.Series, ngram_size:int=1):
-    
-    #On récupère tous les textes nettoyés
-    all_texts = []
-    for text in serie.values:
-        all_texts += text
-    
-    # On cherche les n-gramms
-    all_counts = FreqDist(ngrams(all_texts, ngram_size))
-    
-    
-    most_common_words = {
-        "words" : [],
-        "values" : [],
-    }
-    #On recupere les NUM_COMMON_WORDS mots les plus souvent utilisés
-    most_common_words["words"] = [item[0][0] for item in all_counts.most_common(NUM_COMMON_WORDS)]
-    most_common_words["values"] = [item[1] for item in all_counts.most_common(NUM_COMMON_WORDS)]
-
-
-    df_common = pd.DataFrame.from_dict(most_common_words, orient="columns")
-    #On fait 
-    table_words_used = serie.apply(lambda x: [word for word in x if word in most_common_words])
-
-    table_common_words = [[common_word in words for common_word in most_common_words.get("words")] for words in table_words_used.values]
-    
-    #On créé un dataframe avec table_common_words comme valeurs
-    df = pd.DataFrame(
-        table_common_words,
-        columns=most_common_words.get("words"),
-        index = serie.index
-    )
-
-    return df, df_common
-
-
-def stemmatize_words(words:list, lang:str):
-
-    if lang == "fr":
-        stemmed = [FrenchStemmer().stem(word) for word in words]
-
-    elif lang == "en":
-        stemmed = [PorterStemmer().stem(word) for word in words]
-
-    else:
-        stemmed = words
-
-    #On renvoie la liste des mots sans duplicats
-    return list(set(stemmed))
-
-@commons.timeit
-def stemmatize_serie(df):
-    return df.swifter.apply(lambda x: stemmatize_words(x.text_clean, x.lang), axis=1)
-
-#Suppression des stopwords et text -> word
-def remove_stops(text:str, lang):
-
-
-
-    if lang in languages.keys():
-        stopWords = set(stopwords.words(languages.get(lang)))
-        
-        words = word_tokenize(text)
-        wordsFiltered = []
-
-        for w in words:
-            if w not in stopWords:
-                wordsFiltered.append(w)
-
-        return wordsFiltered
-    else:
-        return []
-
-#Trouver les racines des mots
-def lemmatize_text(words, lemmatizer=WordNetLemmatizer()):
-    return [lemmatizer.lemmatize(w) for w in words]
-
-#Suppression de la ponctuation (sauf ')
-@commons.timeit
-def remove_punctuation(serie:pd.Series):
-    return serie.apply(lambda text: re.sub(r"[^\w\s']", ' ', text))
-
-#Suppression des balises
-@commons.timeit
-def remove_balise(serie:pd.Series):
-    return serie.apply(lambda text : re.sub('<[^<]+?>', '', text))
-
-#Suppression des nombres
-@commons.timeit
-def remove_numerical(serie:pd.Series):
-    return serie.apply(lambda text : re.sub(r'[0-9]+', '', text))
-
-#Suppression des mots courts
-def remove_short_words(words:list, length_min:int=2):
-    return [word for word in words if len(word) >=  length_min]
-
-#Passage en minuscule
-@commons.timeit
-def make_lowercase(serie:pd.Series):
-    return serie.apply(lambda text : text.lower())
 
 #Langue d'un texte
-def get_lang(text:str):
+def get_lang(text:str, text_length=300):
     
-    max_length = min(200, len(text), ) #Limitation de longueur
+    if text_length > 0:
+        max_length = min(text_length, len(text), ) #Limitation de longueur
+        text = text[:max_length]
 
     try:
-        return detect(text=text[:max_length], low_memory=True).get("lang")
+        return detect(text=text, low_memory=True).get("lang")
     except Exception as exce:
         return "unkown"
 
-# Detection de langue
-@commons.timeit
-def find_language(serie:pd.Series):
-    return serie.apply(lambda text : get_lang(text))
-
-# Longueur d'un texte
-def get_text_length(text:str):
+# Nombre de mots d'un texte
+def get_num_words(text:str):
     try:
-        return len(text)
+        return len(text.split())
     except Exception as exce:
         return 0
-
-# Nombre de mots d'un texte
-def get_text_words(text:str):
-    try:
-        return text.split()
-    except Exception as exce:
-        return []
 
 # Lecture de fichier csv
 @commons.timeit
