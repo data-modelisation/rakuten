4d9312acc113fefbd7376e6e2bce88d8c95730e2
karolus-git <108416147+karolus-git@users.noreply.github.com>
2023-02-03 14:28:00 +0100

    Mise en place de pipeline pour image

--- src/tools/image.py
+++ src/tools/image.py
@@ -7,64 +7,90 @@ from pytesseract import pytesseract
 import pandas as pd
 import string
 import seaborn as sns
+import multiprocessing as mp
+
+import skimage
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
 from . import commons
-import multiprocessing as mp
 
-NUM_SAMPLES = 1000
+def read_and_resize(link, width=100, height=100):
+    im = imread(link)
+    im = resize(im, (width, height, 3))
+    return im.reshape(-1,3)
 
+def read_images(links):
 
-#Lecture de fichier image
-@commons.timeit
-def read_image_files(pid:pd.Series, iid=pd.Series, folder="", ):
-
-    names = "/image_" + iid.map(str) + "_product_" + pid.map(str) + ".jpg"
-    links = folder + names
-
-    df_image = pd.DataFrame.from_dict({
-        "links" : links,
-    })
-
-    return df_image
-
-# Feature-engineering du texte
-@commons.timeit
-def apply_feature_engineering(df:pd.DataFrame, folder=""):
-
-    #Parallisation de l'extraction d'infos
-    pool = mp.Pool(mp.cpu_count())
-    infos = [pool.apply(get_image_infos, folder+link) for link in df.links.values]
-    results = np.array(infos)
-    pool.close() 
+    images = np.array([read_and_resize(link) for link in links])
     
-    #Le taux de blanc est la 1ere colonne de results
-    df["w_ratios"] = results[:, 0]
+    return images
 
-    # Les trois moyennes des canaux sont les colonnes suivantes
-    df[["r_means", "b_means", "g_means"]] = np.array(results[:, 1:])
-    df[""]
-    return df
+#Transformeur pour passer de RGB à Gray
+class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
+ 
+    def __init__(self):
+        pass
+ 
+    def fit(self, X, y=None):
+        return self
+ 
+    def transform(self, X, y=None):
+        return np.array([skimage.color.rgb2gray(img) for img in X])
 
-#Pour une photo, on récupère ses infos
-def get_image_infos(*args):
-    link = ''.join(args)
-    im = Image.open(link)
-    w_ratios = get_white_ratio(im)
-    rgb_means = get_channel_means(im)
-    return w_ratios, rgb_means[0], rgb_means[1], rgb_means[2]
+#Transformeur pour recupere des données sur les canaux de l'image
+class ChannelAnalyzer(BaseEstimator, TransformerMixin):
+    def __init__(self, folder="./"):
+        self.folder = folder
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+
+        X_w_ratios = get_white_ratio(X)
+        X_rgb_means = get_channel_means(X)
+        #TODO merger
+        return X_copy
+
+# Construction du pipeline pour le preprocessing
+def build_pipeline_preprocessor():
+
+    preprocessor = Pipeline(steps=[    
+        #('analyzer', ChannelAnalyzer()),
+        ("gray", RGB2GrayTransformer()),
+        ('reduct', PCA(n_components = .9)),
+        ])
+
+    return preprocessor
+
+# Construction du pipeline pour le modèle image
+def build_pipeline_model():
+
+    model = Pipeline(steps=[
+        ('scaler', StandardScaler()),
+        ("classifier", SVC(kernel='rbf') )
+    ])
+
+    return model
 
 #Moyenne des canaux
-def get_channel_means(im:Image):
-    
-    im_array = np.array(im)
-    return im_array.mean(axis=(0,1))
+def get_channel_means(im:np.array):
+    return im.mean(axis=1)
 
 # Ratio de blanc dans l'image
-def get_white_ratio(im:Image):
-    mask_white = np.array(im) == [255, 255, 255]
-    return mask_white.sum() / (im.width * im.height * 3)
+def get_white_ratio(im:np.array):
+    mask_white = im[:,:,:] == [255, 255, 255]
+    return mask_white.sum(axis=(1,2)) / (im.shape[1] * im.shape[2])
 
 # Extraction de texte depuis une image
 def get_text_from_image(im:Image):
