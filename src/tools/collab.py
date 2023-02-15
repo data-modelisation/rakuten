3717aa20818b62706f6b1789183d26773a03f10a
karolus-git <108416147+karolus-git@users.noreply.github.com>
2023-02-15 13:07:23 +0100

    Compatibilité collab

---
+++ src/tools/collab.py
@@ -0,0 +1,8 @@
+from google.colab import drive
+import sys
+drive.mount('/content/drive')
+sys.path.insert(0,"/content/drive/MyDrive/Rakuten")
+
+!pip install unidecode
+!pip install fasttext
+!pip install googletrans
