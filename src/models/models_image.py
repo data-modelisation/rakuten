a9e46e358fb80f3c3b5f6d4fd556a2aa5a523c4b
Olga <otolstolutska@gmail.com>
2023-03-06 10:49:44 +0100

    Ajout des modèles image mobilenet et efficientnet

--- src/models/models_image.py
+++ src/models/models_image.py
@@ -1,7 +1,15 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, GlobalAveragePooling2D, Dropout, Flatten, Dense, Cropping2D
+
 from keras.applications.vgg16 import VGG16 
 from keras.applications.vgg16 import preprocess_input as preprocess_input_vgg16
+
+from keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as mobilenet_v2_preprocess_input
+
+from tensorflow.keras.applications.efficientnet import EfficientNetB1
+from tensorflow.keras.applications.efficientnet import preprocess_input as efficientnet_preprocess_input
+
 from sklearn.svm import SVC
 
 from src.models.models_utils import METRICS
@@ -143,3 +151,91 @@ class ModelImage_VGG16_Transfer(ModelImage):
 
     def init_model(self,):
         pass
+
+class ModelImage_EfficientNetB1(ModelImage):
+    def __init__(self, 
+        *args,
+        **kwargs):
+        
+        self.name="image_EfficientNetB1"
+        self.model_neural = True
+        self.clf_parameters = {}
+        self.preprocess_parameters = {}
+
+        super().__init__(*args, **kwargs)
+
+    def init_model(self,):
+        n_class = 27
+        model = Sequential()
+        base_model = EfficientNetB1(
+                                include_top = False,
+                                weights = 'imagenet')
+        # Freezer les couches du EfficientNetB1  
+        for layer in base_model.layers:   
+            layer.trainable = False  
+        
+        model.add(base_model)  
+        model.add(GlobalAveragePooling2D())
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(rate=0.2))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(rate=0.2))
+        model.add(Dense(n_class, activation='softmax'))
+
+        model.compile(
+                    loss="sparse_categorical_crossentropy",
+                    optimizer="adam",
+                    metrics=METRICS
+            )
+
+        return model
+    
+    def get_preprocessor(self):
+        return None#build_pipeline_preprocessor(**self.preprocess_parameters)
+
+    def get_preprocess_input(self):
+        return efficientnet_preprocess_input     
+
+class ModelImage_MobileNetV2(ModelImage):
+    def __init__(self, 
+        *args,
+        **kwargs):
+        
+        self.name="image_MobileNetV2"
+        self.model_neural = True
+        self.clf_parameters = {}
+        self.preprocess_parameters = {}
+
+        super().__init__(*args, **kwargs)
+
+    def init_model(self,):
+        n_class = 27
+        model = Sequential()
+        base_model = MobileNetV2(
+                                include_top = False,
+                                weights = 'imagenet')
+        # Freezer les couches du MobileNetV2  
+        for layer in base_model.layers:   
+            layer.trainable = False  
+        
+        model.add(base_model)  
+        model.add(GlobalAveragePooling2D())
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(rate=0.2))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(rate=0.2))
+        model.add(Dense(n_class, activation='softmax'))
+
+        model.compile(
+                    loss="sparse_categorical_crossentropy",
+                    optimizer="adam",
+                    metrics=METRICS
+            )
+
+        return model
+    
+    def get_preprocessor(self):
+        return None#build_pipeline_preprocessor(**self.preprocess_parameters)
+
+    def get_preprocess_input(self):
+        return mobilenet_v2_preprocess_input      
