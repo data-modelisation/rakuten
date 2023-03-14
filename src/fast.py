24de538307171336c7bf6061fe4b74f3647ea5b9
karolus-git <108416147+karolus-git@users.noreply.github.com>
2023-03-14 12:12:27 +0100

    Suppression constantes

--- src/fast.py
+++ src/fast.py
@@ -14,22 +14,6 @@ app = FastAPI()
 #Origins
 origins = ["https://localhost:8000",]
 
-#Constantes
-BATCH_SIZE_IMAGE = 32
-BATCH_SIZE_TEXT = 64
-EPOCHS_TEXT= 15
-EPOCHS_IMAGE = 5
-EPOCHS_FUSION = 15
-NUM_FOLDS = 3
-NUM_TARGETS = 84916
-TARGET_SHAPE = [100, 100, 3]
-TEST_SPLIT= .16
-RANDOM_STATE = 123
-NUM_TARGETS_TRAIN = int(NUM_TARGETS*(1 - TEST_SPLIT))
-NUM_TARGETS_TEST = int(NUM_TARGETS*(TEST_SPLIT))
-VOCAB_SIZE = 10000
-MAX_SEQUENCE_LENGTH = 250
-MAX_TOKENS=10000
 
 #Modeles 
 model_text = ModelText_Neural_Simple(
@@ -74,6 +58,7 @@ def pred_image(image_url: str):
     response = model_image.predict(
         [UPLOADED_PATH,], 
         generator=data_generator,
+        #model=model_image.model,
         for_api=True,
         is_="image"
     )
@@ -84,7 +69,7 @@ def pred_image(image_url: str):
 def text_prediction(input):
     
     response = model_text.predict(
-        input.split(";"), 
+        [input.split(";")[0],], 
         generator=data_generator,
         for_api=True,
         is_="text"
