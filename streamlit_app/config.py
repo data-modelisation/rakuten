9fbe2aaa048bdaa216f939f1cb264ca93d88117d
karolus-git <108416147+karolus-git@users.noreply.github.com>
2023-03-19 15:00:14 +0100

    Update names

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -10,12 +10,9 @@ from member import Member
 TITLE = "My Awesome App"
 
 TEAM_MEMBERS = [
-    Member(
-        name="John Doe",
-        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
-        github_url="https://github.com/charlessutton",
-    ),
-    Member("Jane Doe"),
+    Member("Olga TOLSTOLUTSKA"),
+    Member("Mohamed BACHKAT"),
+    Member("Charly LAGRESLE"),
 ]
 
-PROMOTION = "Promotion Bootcamp Data Scientist - April 2021"
+PROMOTION = "Promotion Bootcamp Data Scientist - DEC 2022"
